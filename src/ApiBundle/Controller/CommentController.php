912a49a5c36b0bbb898f45f7876c93bfaaddc772
bourgoin <bourgoin.theo1@gmail.com>
2017-06-30 16:55:59 +0200

    add postComment method

--- src/ApiBundle/Controller/CommentController.php
+++ src/ApiBundle/Controller/CommentController.php
@@ -2,7 +2,7 @@
 
 namespace ApiBundle\Controller;
 
-use AppBundle\Entity\Project;
+use AppBundle\Entity\Comment;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -15,12 +15,27 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 class CommentController extends Controller
 {
     /**
-     * @Rest\Get("/comments/new", name="app_post_comment")
+     * @Rest\Post("/comments/new", name="app_post_comment")
      * @Method("POST")
      * @param Request $request
+     * @return Comment
      */
     public function postCommentAction(Request $request)
     {
-        return dump($request);
+        $em = $this->getDoctrine()->getManager();
+        if (!empty($request->get('author'))
+            && !empty($request->get('message'))
+            && !empty($request->get('project'))
+        ) {
+            $project = $em->getRepository('AppBundle:Project')->find($request->get('project'));
+            $comment = new Comment();
+            $comment->setAuthor($request->get('author'))
+                ->setMessage($request->get('message'))
+                ->setProject($project);
+            $em->persist($comment);
+            $em->flush();
+
+            return $comment;
+        }
     }
 }
